f0aad9d9c4ef6a5e6f4a582a90477f206f2d07a3
Khoi-Ngo <khoingo2003@gmail.com>
2024-05-09 06:58:27 +0700

    config CORS, init project

---
+++ swp/src/main/java/org/swp/controller/DemoController.java
@@ -0,0 +1,12 @@
+package org.swp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hi")
+    public String hi() {
+        return "hi";
+    }
+}
