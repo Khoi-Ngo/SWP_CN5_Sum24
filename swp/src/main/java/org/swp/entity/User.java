3c2dfe580500d7d81242f73aca0a177a0e2d2170
Khoi-Ngo <khoingo2003@gmail.com>
2024-05-13 17:16:30 +0700

    recreating authen for backend

--- swp/src/main/java/org/swp/entity/User.java
+++ swp/src/main/java/org/swp/entity/User.java
@@ -1,13 +1,23 @@
 package org.swp.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.swp.enums.UserRole;
 
 
-
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
