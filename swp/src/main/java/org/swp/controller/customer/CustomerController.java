87c2fa9b5f28e7498d30429ddea61f83f965ecfd
Khoi-Ngo <khoingo2003@gmail.com>
2024-05-16 07:01:32 +0700

    rename

--- swp/src/main/java/org/swp/controller/customer/CustomerController.java
+++ swp/src/main/java/org/swp/controller/customer/CustomerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/customer")
-public class Customer {
+public class CustomerController {
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello World");
     }
